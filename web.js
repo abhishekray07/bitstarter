5156295199a8a2f1ef42dfea5caca9935b1bcd2c
Abhishek Ray <abhishekray07@gmail.com>
2013-07-11 23:36:50 +0800

    changed web js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fileData = fs.readFileSync('index.html');
+var fileStr = file.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fileStr);
 });
 
 var port = process.env.PORT || 5000;
